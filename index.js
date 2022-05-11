f8cb92ca7dd225349ff68d1da62261baf5e83a6c
gruberinfos <softwareentwickler@gruberinfos.de>
2022-05-11 11:26:22 +0200

    Inhaltsübersicht Details

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ console.log(`Hello Node.js v${process.versions.node}!`);
  * JavaScript Cheat Sheet
  *
  * Datenstrukturen
+ *   - Variablen und Typen
+ *   - Strings
+ *   - Arrays
+ *   - Assoziative Arrays
+ *   - Mengen
+ * 
  * Kontrollfluss
+ *   - If, Switch, ternärer Operator
+ *   - Bedingungen
+ *   - Schleifen
+ *   - Schleifen und Arrays
+ * 
  * Funktionen
+ *   - Deklarationen, Parameter, Rückgabe
+ *   - Anonyme Methoden
+ *   - Arrays durchlaufen mit forEach, forIn
+ * 
  * Objekte
+ *   - Objekte und Eigenschaften
+ *   - Methoden
+ *   - Konstruktoren
+ * 
+ * weitere Themen:
+ * Module
+ * OOP
  *
  *
  */
