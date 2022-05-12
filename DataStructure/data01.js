5d4affb3b98b63546d9802517e45b9ff75b49992
gruberinfos <softwareentwickler@gruberinfos.de>
2022-05-12 17:10:43 +0200

    Array - deklaration

---
+++ DataStructure/data01.js
@@ -0,0 +1,14 @@
+/**
+ * Arrays
+ * - Deklaration
+ * - Initialisierung
+ * - Zuweisung, Zugriff
+ */
+
+function main() {
+  a1 = [];
+  console.log('Array a1: ' + a1);
+  console.log('Länge von a1: ' + a1.length);
+}
+
+main();
