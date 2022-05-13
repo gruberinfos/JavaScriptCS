728bcf36294a12f544333697b0c065a50a777ae9
gruberinfos <softwareentwickler@gruberinfos.de>
2022-05-13 16:44:48 +0200

    Objekte

--- DataStructure/data01.js
+++ DataStructure/data01.js
@@ -5,11 +5,12 @@
  * - Zuweisung, Zugriff
  */
 
-function A1() {
+let A1 = function () {
   // Array Deklaration
-  a1 = [];
-  console.log('Array a1: ' + a1);
+  let a1 = [];
+  console.log('Type: ' + typeof a1);
   console.log('Länge von a1: ' + a1.length);
+  console.log('Array a1: ' + a1);
 
   // Zuweisung
   a1[0] = '4711';
@@ -20,12 +21,47 @@ function A1() {
     'aäb cde fgh ijk lmn oöp qrsß tuü vwx yz AÄBC DEF GHI JKL MNO ÖPQ RST UÜV WXYZ !"§ $%& /() =?* \'<> #|; ²³~ @`´ ©«» ¼× {}';
 
   // Zugriff
-}
+  console.log('Länge von a1: ' + a1.length);
+  for (let idx = 0; idx < a1.length; idx++) {
+    console.log('Element ' + (idx + 1) + ': ' + a1[idx]);
+  }
 
-function A2() {}
+  // Initialisierung
+};
+
+let A2 = function () {
+  // Assoziative Array
+
+  // Deklaration
+  let a1 = {};
+  console.log('Type: ' + typeof a1);
+  console.log('Länge A2: ' + a1.length);
+  console.log('Array A2: ' + a1);
+
+  // Zuweisung
+  a1['foo'] = 4711;
+  a1['bar'] = true;
+  a1[7] = 'Text';
+
+  console.log('Type: ' + typeof a1);
+  console.log('Länge A2: ' + a1.length);
+  console.log(a1);
+
+  // Zugriff
+
+  // Initialisierung
+};
+
+let A3 = function () {
+  let a1 = { Harry: 123, Sally: 456, Betty: 789 };
+
+  console.log(a1);
+};
 
 function main() {
   A1();
+  A2();
+  A3();
 }
 
 main();
---
+++ Kontrollfluss/loops.js
@@ -0,0 +1,18 @@
+/**
+ * for Schleife
+ */
+console.log('for-loop');
+for (let idx = 0; idx < 10; idx++) {
+  console.log(idx);
+}
+
+console.log('');
+/**
+ * while Schleife
+ */
+console.log('while-loop');
+let count = 0;
+while (count < 10) {
+  count++;
+  console.log(count);
+}
